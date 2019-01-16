ebe2e58412938c4ee0e8b908bebe0c2104edcdff
wangchengming <17863107533@163.com>
2019-01-16 12:30:05 +0800

    用户Schema

--- Schema/user.js
+++ Schema/user.js
@@ -2,7 +2,11 @@ const {Schema} = require("./config.js")
 
 const UserSchema = new Schema({
     username:String,
-    password:String
+    password:String,
+    avatar:{
+        type:String,
+        default:"/avatar/default.jpg"
+    }
 },{versionKey:false})
 
 
