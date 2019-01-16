d1eafd687097b964ccb406f4747c0a3cf44a9ffe
wangchengming <17863107533@163.com>
2019-01-16 23:43:21 +0800

    后台页面

---
+++ control/admin.js
@@ -0,0 +1,54 @@
+const {db} = require("../Schema/config.js")
+//通过db对象创建操作article数据库的模型对象
+const ArticleSchema = require("../Schema/article.js")
+const Article = db.model("articles",ArticleSchema)
+//取用户的Schema，为了拿到操作users集合的实例对象
+const UserSchema = require("../Schema/user.js")
+const User = db.model("users",UserSchema)
+
+
+const CommentSchema = require("../Schema/comment.js")
+const Comment = db.model("comments",CommentSchema)
+
+const fs = require("fs")
+const {join} = require("path")
+
+
+
+exports.index = async ctx => {
+    console.log(ctx.session)
+    if(ctx.session.isNew){
+        //没有登录
+        ctx.status = 404;
+        return await ctx.render("404.pug",{title:"404"})
+    }
+    const id = ctx.params.id;
+    const arr = fs.readdirSync(join(__dirname,"../views/admin"))
+
+    let flag = false;
+
+    arr.forEach(v => {
+        const name = v.replace(/^(admin\-)|(\.pug)$/g,"");
+        if(name === id){
+            flag = true;
+        }
+    })
+    if(flag){
+        await ctx.render("./admin/admin-" + id + ".pug",{
+            role:ctx.session.role
+        })
+    }else{
+        ctx.status = 404;
+        await ctx.render("404.pug",{title:"404"})
+    }
+}
+
+
+
+
+
+
+
+
+
+
