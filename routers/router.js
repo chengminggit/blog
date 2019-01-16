12a29450b4bfa4ff38634029b51aa875409b9fe9
wangchengming <17863107533@163.com>
2019-01-16 12:25:01 +0800

    路由

--- routers/router.js
+++ routers/router.js
@@ -1,17 +1,11 @@
 const Router = require("koa-router");
 //拿到操作user表的逻辑对象
 const user = require("../control/user.js")
+const article = require("../control/article.js")
 const router = new Router();
 
 //设计主页
-router.get('/', async (ctx) => {
-    await ctx.render("index.pug",{
-        // session:{
-        //     role:666
-        // },
-        title:"追逐的博客"
-    })
-})
+router.get('/',user.keepLog, article.getList)
 
 //主要用来处理返回 用户登陆 用户注册
 router.get(/^\/user\/(?=reg|login)/, async (ctx) => {
@@ -29,5 +23,16 @@ router.post("/user/reg",user.reg)
 //用户登录路由
 router.post("/user/login",user.login)
 
+//用户退出
+router.get("/user/logout",user.logout)
+
+//文章发表页面
+router.get("/article",user.keepLog,article.addPage)
+
+//文章添加
+router.post("/article",user.keepLog,article.add)
+
+//文章列表分页路由
+router.get("/page/:id",article.getList)
 
 module.exports = router;
